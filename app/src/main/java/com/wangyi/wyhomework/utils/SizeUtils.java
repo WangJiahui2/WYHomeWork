8a25dd74c838e99314c153181da4ad1a3da83c47
wangjiahui <523809205@qq.com>
2022-05-04 12:05:03 +0800

    Revert "（feat1.1.8）测试修改"
    This reverts commit 1ce25c1d7d9997d1a89e5bdb22d73b116b23d0b2.

--- app/src/main/java/com/wangyi/wyhomework/utils/SizeUtils.java
+++ app/src/main/java/com/wangyi/wyhomework/utils/SizeUtils.java
@@ -5,7 +5,6 @@ import android.content.Context;
 public class SizeUtils {
     public static int dip2px(Context context, float dpValue) {
         float scale = context.getResources().getDisplayMetrics().density;
-        
         return (int) (dpValue * scale + 0.5f);
     }
 
